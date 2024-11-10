c9c36968d54c79ad69a70fca89e113adaac3dc7e
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-11-10 20:48:15 +0800

    feat: base64

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ nuxi generate &&
 rm -fr .output/public/{200,404}.html &&
 rm -fr .output/public/_nuxt/error-{404,500}.*.css &&
 rm -fr .output/public/_nuxt/builds &&
-tar cvf - .output/public | xz > build.tar.xz
+tar cvf - .output/public | xz > build.tar.xz &&
+base64 < build.tar.xz > build.tar.xz.base64
